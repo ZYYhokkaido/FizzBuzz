63572f261f51d4b73697cdac0c9fdbae3fb70f65
Yiyang Zhang <YZhang184@slb.com>
2023-11-01 14:40:47 +0800

    test: add test for Fizz, Buzz, FizzBuzz case

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -2,5 +2,24 @@
 {
     public class FizzBuzzGame
     {
+        public static string CountOff(int givenNumber)
+        {
+            if (givenNumber % 3 == 0 && givenNumber % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (givenNumber % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (givenNumber % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return givenNumber.ToString();
+            }
+        }
     }
 }
--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -13,5 +13,32 @@ namespace FizzBuzzTest
             //then
             Assert.Equal("1", res);
         }
+
+        [Fact]
+        public void Should_return_Fizz_when_Fizz_case_given()
+        {
+            int givenNumber = 3;
+            string res = FizzBuzzGame.CountOff(givenNumber);
+            //then
+            Assert.Equal("Fizz", res);
+        }
+
+        [Fact]
+        public void Should_return_Buzz_when_Buzz_case_given()
+        {
+            int givenNumber = 5;
+            string res = FizzBuzzGame.CountOff(givenNumber);
+            //then
+            Assert.Equal("Buzz", res);
+        }
+
+        [Fact]
+        public void Should_return_FizzBuzz_when_FizzBuzz_case_given()
+        {
+            int givenNumber = 15;
+            string res = FizzBuzzGame.CountOff(givenNumber);
+            //then
+            Assert.Equal("FizzBuzz", res);
+        }
     }
 }
